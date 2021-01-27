572dd8339a565cdb4bf8a62f9dc7b2c1bab03978
Sean Hwang <hwang103@purdue.edu>
2021-01-27 16:47:49 -0500

    installed webcam

--- cpu.py
+++ cpu.py
@@ -4,22 +4,24 @@ import numpy as np
 faceCascade = cv2.CascadeClassifier("cascades/haarcascade_frontalface_default.xml")
 catCascade = cv2.CascadeClassifier("cascades/haarcascade_frontalcatface_extended.xml")
 
-img = cv2.imread("images/family.PNG")
+cap = cv2.VideoCapture(0)
 
-# cap = cv2.VideoCapture(0)
+while True:
+    success, img = cap.read()
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    faces = faceCascade.detectMultiScale(imgGray, 1.2, 4)
+    catface = catCascade.detectMultiScale(imgGray, 1.1, 4)
 
-imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = faceCascade.detectMultiScale(imgGray, 1.2, 4)
-catface = catCascade.detectMultiScale(imgGray, 1.1, 4)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        cv2.putText(img,"Human",(x+w+5,y+h),cv2.FONT_HERSHEY_COMPLEX,0.5,(0,255,0))
 
-for (x,y,w,h) in faces:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-    cv2.putText(img,"Human",(x+w+5,y+h),cv2.FONT_HERSHEY_COMPLEX,0.5,(0,255,0))
+    for (x,y,w,h) in catface:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+        cv2.putText(img,"Cat",(x+w+5,y+h),cv2.FONT_HERSHEY_COMPLEX,0.5,(0,255,0))
 
-for (x,y,w,h) in catface:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
-    cv2.putText(img,"Cat",(x+w+5,y+h),cv2.FONT_HERSHEY_COMPLEX,0.5,(0,255,0))
+    cv2.imshow("name of window", img)
 
-cv2.imshow("Result", img)
-cv2.waitKey(0)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
