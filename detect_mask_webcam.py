a307bb43715244d23e78be63234c52d67b9eb495
Hayley <sh950911@gmail.com>
2021-03-02 05:47:04 -0500

    Commit on R pi

---
+++ detect_mask_webcam.py
@@ -0,0 +1,127 @@
+# Written by: Sean Hwang
+# COVIDoor - COVID Screening Automatic Door
+# Mask Detector Software
+
+#-------------------------------------#
+
+# import the necessary packages
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+import os
+
+def detect_and_predict_mask(frame, faceNet, maskNet):
+	(h, w) = frame.shape[:2]
+	blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
+		(104.0, 177.0, 123.0))
+
+	faceNet.setInput(blob)
+	detections = faceNet.forward()
+
+	faces = []
+	locs = []
+	preds = []
+
+	# loop over the detections
+	for i in range(0, detections.shape[2]):
+		confidence = detections[0, 0, i, 2]
+
+		if confidence > args["confidence"]:
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
+
+			(startX, startY) = (max(0, startX), max(0, startY))
+			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+			# extract the face ROI, convert it from BGR to RGB channel
+			face = frame[startY:endY, startX:endX]
+			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+			face = cv2.resize(face, (224, 224))
+			face = img_to_array(face)
+			face = preprocess_input(face)
+
+			faces.append(face)
+			locs.append((startX, startY, endX, endY))
+
+	if len(faces) > 0:
+		faces = np.array(faces, dtype="float32")
+		preds = maskNet.predict(faces, batch_size=32)
+
+	return (locs, preds)
+
+# argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-f", "--face", type=str,
+	default="face_detector",
+	help="path to face detector model directory")
+ap.add_argument("-m", "--model", type=str,
+	default="mask_detector.model",
+	help="path to trained face mask detector model")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+
+print("Welcome! Launching COVIDoor...")
+time.sleep(2.0)
+
+# load our serialized face detector model from disk
+print("[COVIDoor] loading face detector model...")
+prototxtPath = os.path.sep.join([args["face"], "deploy.prototxt"])
+weightsPath = os.path.sep.join([args["face"],
+	"res10_300x300_ssd_iter_140000.caffemodel"])
+faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+# load the face mask detector model from disk
+print("[COVIDoor] loading mask detector model from neural network...")
+maskNet = load_model(args["model"])
+
+# initialize the video stream and allow the camera sensor to warm up
+print("[COVIDoor] starting video ...")
+
+vs = VideoStream(src=0).start()
+time.sleep(1.0)
+
+print("[COVIDoor] System On.")
+
+# loop over the frames from the video stream
+while True:
+	frame = vs.read()
+	frame = imutils.resize(frame, width=500)
+
+	(locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+
+	for (box, pred) in zip(locs, preds):
+		# unpack the bounding box and predictions
+		(startX, startY, endX, endY) = box
+		(mask, withoutMask) = pred
+
+		if mask > withoutMask:
+			label = "Mask ON"
+			print("[COVIDoor] Mask has been Detected || mask_on == '1' ")
+			color = (0, 255, 0)
+
+		else:
+			label = "Mask OFF. Please wear a mask to enter"
+			print("[COVIDoor] No mask!! || mask_on == '0' ")
+			color = (0, 0, 255)
+		
+		cv2.putText(frame, label, (startX-50, startY - 10),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
+		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+
+	# show the output frame
+	cv2.imshow("Face Mask Detector", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	if key == ord("q"):
+		break
+
+cv2.destroyAllWindows()
+vs.stop()
