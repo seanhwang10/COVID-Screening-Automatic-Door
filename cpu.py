3ceb12d4d1fa05317f034bbf65fefea252fffa97
Sean Hwang <hwang103@purdue.edu>
2021-03-04 07:25:31 -0500

    update UI for temperature checker

--- cpu.py
+++ cpu.py
@@ -1,27 +1,22 @@
 import cv2
 import numpy as np
 
-faceCascade = cv2.CascadeClassifier("cascades/haarcascade_frontalface_default.xml")
-catCascade = cv2.CascadeClassifier("cascades/haarcascade_frontalcatface_extended.xml")
-
 cap = cv2.VideoCapture(0)
 
 while True:
     success, img = cap.read()
-    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    faces = faceCascade.detectMultiScale(imgGray, 1.2, 4)
-    catface = catCascade.detectMultiScale(imgGray, 1.1, 4)
-
-    for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-        cv2.putText(img,"Human",(x+w+5,y+h),cv2.FONT_HERSHEY_COMPLEX,0.5,(0,255,0))
-
-    for (x,y,w,h) in catface:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
-        cv2.putText(img,"Cat",(x+w+5,y+h),cv2.FONT_HERSHEY_COMPLEX,0.5,(0,255,0))
-
-    cv2.imshow("name of window", img)
-
+    cv2.circle(img, (330, 250), 180, (0, 255, 5), 3) #center circle
+    cv2.rectangle(img, (0, 0), (img.shape[1], 50), (0, 0, 0), -1) #filled textline
+    cv2.putText(img, "Please fit your face to the green circle for temperature measurement", (0, 30), cv2.FONT_ITALIC, 0.55, (0, 255, 0), 1) #font
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        cv2.putText(img, "Temperature: 36.5C", (5, 120), cv2.FONT_ITALIC, 0.6, (0, 255, 0), 2)  # font
+    else:
+        cv2.putText(img, "Temperature: 39.3C", (5, 120), cv2.FONT_ITALIC, 0.6, (0, 0, 255), 2)  # font
+
+
+    cv2.imshow("display", img)
+    #
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #     break
+
