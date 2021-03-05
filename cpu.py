74989ad3efd7381379733afdfbd9ab8cd2abc2d7
Sean Hwang <hwang103@purdue.edu>
2021-03-05 02:33:02 -0500

    Update cpu.py

--- cpu.py
+++ cpu.py
@@ -11,7 +11,7 @@ while True:
     cv2.putText(img, "Please fit your face to the green circle for temperature measurement", (0, 30), cv2.FONT_ITALIC, 0.55, (0, 255, 0), 1) #font
 
     # Test values for now..
-    # Temp values will be measured from IR sensor 
+    # Temp values will be measured from IR sensor
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.putText(img, "Temperature: 36.5C", (5, 120), cv2.FONT_ITALIC, 0.6, (0, 255, 0), 2)  # font
     else:
