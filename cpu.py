afe9b2df5e1fc2c123c2301109fe4001e818ad29
Sean Hwang <hwang103@purdue.edu>
2021-04-12 01:42:35 -0400

    add servo module

--- cpu.py
+++ cpu.py
@@ -6,18 +6,18 @@ cap = cv2.VideoCapture(0)
 while True:
     success, img = cap.read()
 
-    cv2.circle(img, (330, 250), 180, (0, 255, 5), 3) #center circle
-    cv2.rectangle(img, (0, 0), (img.shape[1], 50), (0, 0, 0), -1) #filled textline
-    cv2.putText(img, "Please fit your face to the green circle for temperature measurement", (0, 30), cv2.FONT_ITALIC, 0.55, (0, 255, 0), 1) #font
-
+    #cv2.circle(img, (330, 250), 180, (0, 255, 5), 3) #center circle
+    #cv2.rectangle(img, (0, 0), (img.shape[1], 50), (0, 0, 0), -1) #filled textline
+    #cv2.putText(img, "Please fit your face to the green circle for temperature measurement", (0, 30), cv2.FONT_ITALIC, 0.55, (0, 255, 0), 1) #font
+    cv2.putText(img, "IR sensor test", (150, 30), cv2.FONT_ITALIC, 2, (0, 255, 0), 1)  # font
     # Test values for now..
     # Temp values will be measured from IR sensor
-    cv2.putText(img, "Temp: 36.5C (test)", (460, 120), cv2.FONT_ITALIC, 0.6, (0, 255, 0), 2)  # font
+    cv2.putText(img, "Temp: 34.1C", (460, 120), cv2.FONT_ITALIC, 0.6, (0, 255, 0), 2)  # font
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        cv2.putText(img, "Checking Temp...", (5, 120), cv2.FONT_ITALIC, 0.6, (0, 255, 0), 2)  # font
-    else:
-        cv2.putText(img, "Please come closer!", (5, 120), cv2.FONT_ITALIC, 0.6, (0, 0, 255), 2)  # font
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #     cv2.putText(img, "Checking Temp...", (5, 120), cv2.FONT_ITALIC, 0.6, (0, 255, 0), 2)  # font
+    # else:
+    #     cv2.putText(img, "Please come closer!", (5, 120), cv2.FONT_ITALIC, 0.6, (0, 0, 255), 2)  # font
 
 
     cv2.imshow("display", img)
